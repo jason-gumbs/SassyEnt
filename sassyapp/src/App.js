4405899a29e1d53b47f029fca6a33cdac35e1a7a
BEAZERS BUSINESS LLC <wilfred.smithandsmith@gmail.com>
2019-01-13 13:56:55 -0500

    all pink

--- sassyapp/src/App.js
+++ sassyapp/src/App.js
@@ -1,25 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem,
+  Navbar,
+  Grid,
+  Row,
+  Col
+} from "react-bootstrap";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">Sassy the cosmetologist</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav
+            bsStyle="tabs"
+            onSelect={k => this.handleSelect(k)}
+            className="nav"
+            pullRight
           >
-            Learn React
-          </a>
-        </header>
+            <NavItem eventKey="1" href="/home">
+              Home
+            </NavItem>
+            <NavDropdown eventKey="4" title="Services" id="nav-dropdown">
+              <MenuItem eventKey="4.1">Action</MenuItem>
+              <MenuItem eventKey="4.2">Another action</MenuItem>
+              <MenuItem eventKey="4.3">Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey="4.4">Separated link</MenuItem>
+            </NavDropdown>
+            <NavItem eventKey="3">Product</NavItem>
+            <NavItem eventKey="4">About</NavItem>
+          </Nav>
+        </Navbar>
+        <Grid fluid style={{ hieght: "100%" }}>
+          <Row>
+            <Col />
+            <Col style={{ margin: "auto" }}>
+              <h1 className="cursive">Coming Soon</h1>
+              <p>NEWS LETTER SIGN-UP AND PROMOTIONS</p>
+            </Col>
+            <Col />
+          </Row>
+        </Grid>
       </div>
     );
   }
